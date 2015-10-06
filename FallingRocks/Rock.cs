acf6909bf391ed437bed18ea8d24cd7aba6e8733
ktodorovv <kiril.todorov@gmail.com>
2015-10-06 21:45:43 +0300

    Update Rock.cs

--- FallingRocks/Rock.cs
+++ FallingRocks/Rock.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,22 +17,22 @@ namespace FallingRocks
 
         private ConsoleColor _color;
         private char _symbol;
-        private int _boundaryX;
-        private int _boundaryY;
         private Random random = new Random();
         
         public bool isVisible;
 
         public Rock(int boundaryX, int boundaryY)
         {
-            _boundaryX = boundaryX;
-            _boundaryY = boundaryY;
+            this.BoundaryX = boundaryX;
+            this.BoundaryY = boundaryY;
             isVisible = true;
             x = random.Next(0, boundaryX); //we are generating random x coordinate for the rock
             y = 0; // y is always 0 because the rock starts falling from the top
             _symbol = _possibleSymbols[random.Next(0, _possibleSymbols.Count)]; // getting random symbol from the array
             _color = _possibleColors[random.Next(0, _possibleColors.Count)]; // same for color
         }
+        public int BoundaryX { get; set; }
+        public int BoundaryY { get; set; }
 
         public void MoveDown()
         {
